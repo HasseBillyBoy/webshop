2a139eb256ae3f8dd9a4b4650163db0bda3e8996
Hasse <haje0001@stud.kea.dk>
2023-08-31 13:08:55 +0200

    commit med lidt af hvert

--- js/product.js
+++ js/product.js
@@ -19,9 +19,20 @@ function showProduct(product) {
   console.log(product);
   document.querySelector(".horizontalhalf h2").textContent =
     product.productdisplayname;
+
   document.querySelector(
     "img:nth-child(2)"
   ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
-  document.querySelector(".horizontalhalf p:nth-child(2)").textContent =
-    product.price;
+
+  document.querySelector(".horizontalhalf p").textContent =
+    "Brand: " + product.brandname;
+
+  document.querySelector(".horizontalhalf p:nth-child(3)").textContent =
+    "Price: " + product.price + ",-";
+
+  document.querySelector(".horizontalhalf p:nth-child(4)").textContent =
+    "Discount: " + product.discount + "%";
+
+  document.querySelector(".horizontalhalf p:nth-child(5)").innerHTML =
+    "Om produktet: " + product.description;
 }
